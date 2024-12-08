f5e052453495486bc6e4c22d094c5110d81efd45
Hydration <168127639+tomoriny@users.noreply.github.com>
2024-12-08 16:25:52 +0800

    :sparkles: [wip] reactive

---
+++ packages/core/reactive/context.ts
@@ -0,0 +1,50 @@
+interface InjectionConstraint<_> {}
+export type InjectionKey<T> = symbol & InjectionConstraint<T>
+
+export type Context = Map<symbol, unknown> & { parent?: Context }
+
+export const rootContext = createContext(true)
+let activeContext: Context = rootContext
+
+export function createContext(detached = false): Context {
+  const context = new Map() as Context
+  context.parent = detached ? void 0 : activeContext
+  return context
+}
+
+export function getCurrentContext(): Context {
+  return activeContext
+}
+
+export function runInContext<T>(context: Context, body: () => T): T {
+  const prevContext = activeContext
+  activeContext = context
+  try {
+    const value = body()
+    return value
+  }
+  finally {
+    activeContext = prevContext
+  }
+}
+
+function lookup<T>(key: InjectionKey<T>): Context | undefined {
+  let context: Context | undefined = activeContext
+  while (context != null && !context.has(key)) {
+    context = context.parent
+  }
+
+  return context
+}
+
+export function provide<T>(key: InjectionKey<T>, value: T): void {
+  if (activeContext.has(key)) {
+    throw new TypeError('...')
+  }
+
+  activeContext.set(key, value)
+}
+
+export function inject<T>(key: InjectionKey<T>): T | undefined {
+  return lookup<T>(key)?.get(key) as T
+}
---
+++ packages/core/reactive/effect.ts
@@ -0,0 +1,100 @@
+export type EffectBucket = Set<Effect>
+export interface EffectOptions { scheduler?: EffectScheduler, lazy?: boolean }
+export type EffectScheduler = (effect: Effect) => void
+export interface EffectMetadata { history: EffectBucket[], scheduler?: EffectScheduler, scope?: EffectScope }
+export type Effect = EffectMetadata & (() => unknown)
+
+export interface EffectScope {
+  children: EffectScope[]
+  effects: Set<Effect>
+}
+
+let activeEffect: Effect | undefined = void 0
+let activeScope: EffectScope | undefined = void 0
+let shouldTrack = true
+
+export function getCurrentEffect(): Effect | undefined {
+  return activeEffect
+}
+
+export function getCurrentScope(): EffectScope | undefined {
+  return activeScope
+}
+
+export function createScope(detached = false): EffectScope {
+  const scope: EffectScope = { children: [], effects: new Set() }
+  if (!detached && activeScope)
+    activeScope.children.push(scope)
+  return scope
+}
+
+export function runInScope(scope: EffectScope, body: () => void): void {
+  const prevScope = activeScope
+  activeScope = scope
+  try {
+    body()
+  }
+  finally {
+    activeScope = prevScope
+  }
+}
+
+export function clearScope(scope: EffectScope): void {
+  scope.children.forEach(v => clearScope(v))
+  scope.effects.forEach(v => cleanup(v))
+  scope.effects.clear()
+}
+
+export function cleanup(e: Effect): void {
+  e.history.forEach(i => i.delete(e))
+  e.history.length = 0
+  e.scope?.effects.delete(e)
+}
+
+export function effect(fn: () => unknown, { lazy = false, scheduler }: EffectOptions = {}): Effect {
+  const e = (() => {
+    cleanup(e)
+    const prevEffect = activeEffect
+    activeEffect = e
+    try {
+      const value = fn()
+      return value
+    }
+    finally {
+      activeEffect = prevEffect
+    }
+  }) as Effect
+
+  e.history = []
+  e.scheduler = scheduler
+  e.scope = activeScope
+
+  if (activeScope)
+    activeScope.effects.add(e)
+
+  if (!lazy)
+    e()
+
+  return e
+}
+
+export function track(bucket: EffectBucket): void {
+  if (!activeEffect || !shouldTrack)
+    return
+  bucket.add(activeEffect)
+  activeEffect.history.push(bucket)
+}
+
+export function trigger(bucket: EffectBucket): void {
+  const effectsToRun: Effect[] = []
+  bucket.forEach(v => v !== activeEffect ? effectsToRun.push(v) : void 0)
+  effectsToRun.forEach(v => (v.scheduler ?? v)(v))
+}
+
+export function pauseTracking(): void {
+  shouldTrack = false
+}
+
+export function resetTracking(): void {
+  shouldTrack = true
+}
